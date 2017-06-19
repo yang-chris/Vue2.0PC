bc00856122300756f86e92703b4f9e2aae5df6d8
yang-chris <18813068824@163.com>
2017-06-19 22:59:45 +0800

    详情页

---
+++ src/pages/detail/Analysis.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+      分析
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus"></style>
