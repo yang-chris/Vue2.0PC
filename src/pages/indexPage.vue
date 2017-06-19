59e3ade153d063c2ba69173c782e0f62f3824e88
yang-chris <18813068824@163.com>
2017-06-19 22:54:50 +0800

    详情页

--- src/pages/indexPage.vue
+++ src/pages/indexPage.vue
@@ -32,7 +32,7 @@
           class="index-board-item"
           v-for="(item, index) in boardList"
           :class="[{'line-last' : index % 2 !== 0}, 'index-board-' + item.id]">
-          <div class="index-board-item-inner" >
+          <div class="index-board-item-inner">
             <h2>{{ item.title }}</h2>
             <p>{{ item.description }}</p>
             <div class="index-board-button">
@@ -48,12 +48,12 @@
 <script type="text/ecmascript-6">
   import sliderShow from '../components/sliderShow.vue'
   export default {
-    components:{
+    components: {
       sliderShow
     },
     data(){
       return {
-        slideSpeed:2000,
+        slideSpeed: 2000,
         slides: [
           {
             src: require('../assets/slideShow/pic1.jpg'),
@@ -156,15 +156,15 @@
         }
       }
     },
-    created:function () {
-      this.$http.get('api/getNewsList').then((res)=>{
-          this.newsList=res.body;
+    created: function () {
+      this.$http.get('api/getNewsList').then((res) => {
+        this.newsList = res.body;
       })
     },
-    methods:{
-        doSomething(){
-            //console.log('do something')
-        }
+    methods: {
+      doSomething(){
+        //console.log('do something')
+      }
     }
   }
 </script>
@@ -174,48 +174,59 @@
     margin: 20px auto;
     width: 1000px;
   }
+
   .index-wrap {
     width: 1200px;
     margin: 0 auto;
     overflow: hidden;
   }
+
   .index-left {
     float: left;
     width: 300px;
     text-align: left;
   }
+
   .index-right {
     float: left;
     width: 900px;
   }
+
   .index-left-block {
     margin: 15px;
     background: #fff;
     box-shadow: 0 0 1px #ddd;
   }
+
   .index-left-block .hr {
     margin-bottom: 20px;
   }
+
   .index-left-block h2 {
     background: #4fc08d;
     color: #fff;
     padding: 10px 15px;
     margin-bottom: 20px;
   }
+
   .index-left-block h3 {
     padding: 0 15px 5px 15px;
     font-weight: bold;
     color: #222;
   }
+
   .index-left-block ul {
     padding: 10px 15px;
   }
+
   .index-left-block li {
     padding: 5px;
   }
+
   .index-board-list {
     overflow: hidden;
   }
+
   .index-board-item {
     float: left;
     width: 400px;
@@ -225,41 +236,52 @@
     margin-right: 20px;
     margin-bottom: 20px;
   }
+
   .index-board-item-inner {
     min-height: 125px;
     padding-left: 120px;
   }
-  .index-board-car .index-board-item-inner{
+
+  .index-board-car .index-board-item-inner {
     background: url(../assets/images/1.png) no-repeat;
   }
-  .index-board-loud .index-board-item-inner{
+
+  .index-board-loud .index-board-item-inner {
     background: url(../assets/images/2.png) no-repeat;
   }
-  .index-board-earth .index-board-item-inner{
+
+  .index-board-earth .index-board-item-inner {
     background: url(../assets/images/3.png) no-repeat;
   }
-  .index-board-hill .index-board-item-inner{
+
+  .index-board-hill .index-board-item-inner {
     background: url(../assets/images/4.png) no-repeat;
   }
+
   .index-board-item h2 {
     font-size: 18px;
     font-weight: bold;
     color: #000;
     margin-bottom: 15px;
   }
+
   .line-last {
     margin-right: 0;
   }
+
   .index-board-button {
     margin-top: 20px;
   }
+
   .lastest-news {
     min-height: 512px;
   }
+
   .hot-tag {
     background: red;
     color: #fff;
   }
+
   .new-item {
     display: inline-block;
     width: 230px;
